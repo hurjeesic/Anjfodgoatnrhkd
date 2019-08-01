be7c840334153fab2f5dc629e8d64081259d309f
hurjeesic <hurjeesic@naver.com>
2019-08-01 23:43:07 +0900

    dictionary.html 책에 데이터 넣는 것 구현

--- quiz/views.py
+++ quiz/views.py
@@ -10,29 +10,39 @@ def index(request):
     return render(request, 'quiz/index.html', {})
 
 def quiz(request):
-    if request.method == "POST":
-        Example.objects.all().delete()
-        lst = function.proverb_parsing()
-        me = User.objects.get(username='anjfodgoatnrhkd')
-        for sentence in lst:
-            Example(author=me, sentence=sentence).save()
+    # if request.method == "POST":
+    #     Example.objects.all().delete()
+    #     lst = function.proverb_parsing()
+    #     me = User.objects.get(username='anjfodgoatnrhkd')
+    #     for sentence in lst:
+    #         Example(author=me, sentence=sentence).save()
+    #
+    #     return redirect('index')
 
-        return redirect('index')
+    # examples = Example.objects.all().order_by('sentence')
+    dialects = Dialect.objects.all()
 
-    examples = Example.objects.all().order_by('sentence')
-
-    return render(request, 'quiz/quiz.html', {'examples': examples})
+    return render(request, 'quiz/quiz.html', {'dialects': dialects})
 
 def dictionary(request):
-    if request.method == "POST":
-        Dialect.objects.all().delete()
-        lst = function.dialect_parsing()
-        for lang in lst:
-            Dialect(standard=lang['standard'], jeju=lang['jeju']).save()
+    # if request.method == "POST":
+    #     Dialect.objects.all().delete()
+    #     lst = function.dialect_parsing()
+    #     for lang in lst:
+    #         Dialect(standard=lang['standard'], jeju=lang['jeju']).save()
+    #
+    #     return redirect('index')
 
-        return redirect('index')
-
-    dialects = Dialect.objects.all() # .order_by('standard')
+    datas = Dialect.objects.all() # .order_by('standard')
+    temps = []
+    dialects = []
+    index = 1
+    for data in datas:
+        temps.append({'standard': data.standard, 'jeju': data.jeju})
+        if index % 15 == 0:
+            dialects.append(temps)
+            temps = []
+        index += 1
 
     return render(request, 'quiz/dictionary.html', {'dialects': dialects})
 
