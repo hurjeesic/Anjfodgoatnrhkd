71af7accbab8bfa6b4635272ec31fbab439049a5
hurjeesic <hurjeesic@naver.com>
2019-08-13 01:10:50 +0900

    받는 데이터명 수정 및 관리자 파일 수정

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Example
 from .models import Dialect
+from .models import SensorData
 # from .models import ModelName
 
 # Register your models here.
 admin.site.register(Example)
 admin.site.register(Dialect)
+admin.site.register(SensorData)
 # admin.site.register(ModelName)
--- quiz/views.py
+++ quiz/views.py
@@ -10,7 +10,8 @@ problem_num = 20
 
 # Create your views here.
 def temp(request):
-    sensor = { 'soilTemp' : request.POST.get('soilTemp', '-40'), 'soilHumid' : request.POST.get('soilTemp', '0'), 'waterTemp' : request.POST.get('soilTemp', '-1000'), 'PH' : request.POST.get('PH', '0'), 'voltage' : request.POST.get('voltage', '0') }
+    # AT, H, WT, PH, V
+    sensor = { 'soilTemp' : request.POST.get('AT', '-40'), 'soilHumid' : request.POST.get('H', '0'), 'waterTemp' : request.POST.get('WT', '-1000'), 'PH' : request.POST.get('PH', '0'), 'voltage' : request.POST.get('V', '0') }
 
     try:
         migrationData = SensorData(soilTemp=int(sensor['soilTemp']),
